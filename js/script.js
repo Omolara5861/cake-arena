186e521d77ebceb1bfd5e16b0019ad4190ee9a77
Lara <debbiegterra@gmail.com>
2021-12-20 19:00:33 +0100

    slider section

--- js/script.js
+++ js/script.js
@@ -63,4 +63,37 @@ AOS.init({
     });
     };
     scrollWindow();
-});
+    $.Scrollax();
+
+  //   carousel
+    let carousel = function () {
+        $(".home-slider").owlCarousel({
+        loop: true,
+        autoplay: true,
+        margin: 0,
+        animateOut: "fadeOut",
+        animateIn: "fadeIn",
+        nav: true,
+        dots: false,
+        autoplayHoverPause: false,
+        items: 1,
+        navText: [
+            "<span class = 'ion-ios-arrow-back'></span>",
+            "<span class = 'ion-ios-arrow-forward'></span>",
+        ],
+        responsive: {
+            0: {
+            items: 1,
+            },
+            600: {
+            items: 1,
+            },
+            1000: {
+            items: 1,
+            },
+        },
+        });
+    };
+    carousel();
+
+})(jQuery);
