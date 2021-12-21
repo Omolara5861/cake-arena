c221e9ed05cd573d58fa2f85c5f149f1cff2bbc1
Lara <debbiegterra@gmail.com>
2021-12-21 18:22:03 +0100

    Order a cake section

--- js/script.js
+++ js/script.js
@@ -190,4 +190,9 @@ AOS.init({
     };
     contentWayPoint();
 
+    $("#book_date").datepicker({
+        format: "m/d/yyyy",
+        autoclose: true,
+    });
+    $("#book_time").timepicker();
 })(jQuery);
