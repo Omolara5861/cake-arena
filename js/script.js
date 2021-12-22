346e094a2e387b4e76217ee4c61efd14132b831b
Lara <debbiegterra@gmail.com>
2021-12-22 06:21:50 +0100

    fixed gallery pic zoom and modify submit order button

--- js/script.js
+++ js/script.js
@@ -1,198 +1,206 @@
 // Animation on scroll
 AOS.init({
-    duration: 800,
-    easing: 'slide'
+  duration: 800,
+  easing: "slide",
 });
 
 (function ($) {
-    'use strict';
-    $(window).stellar({
+  "use strict";
+  $(window).stellar({
     responsive: true,
     parallaxBackgrounds: true,
     parallaxElements: true,
     horizontalScrolling: false,
     hideDistantElements: false,
-    scrollProperty: 'scroll'
+    scrollProperty: "scroll",
+  });
+
+  // Full Height
+  let fullHeight = function () {
+    $(".js-fullheight").css("height", $(window).height());
+    $(window).resize(() => {
+      $(".js-fullheight").css("height", $(window).height());
     });
+  };
+  fullHeight();
 
-    // Full Height 
-    let fullHeight = function() {
-        $('.js-fullheight').css('height', $(window).height());
-        $(window).resize( () => {
-         $('.js-fullheight').css('height', $(window).height());
-        });
-    }; 
-    fullHeight();
-
-    // Navbar Scroll
-    let scrollWindow = function() {
-        $(window).scroll( () => {
-    let $w = $(this),
+  // Navbar Scroll
+  let scrollWindow = function () {
+    $(window).scroll(() => {
+      let $w = $(this),
         st = $w.scrollTop(),
         navbar = $(".ftco_navbar"),
         sd = $(".js-scroll-wrap");
-        if (st > 150) {
+      if (st > 150) {
         if (!navbar.hasClass("scrolled")) {
-            navbar.addClass("scrolled");
+          navbar.addClass("scrolled");
         }
-        }
-        if (st < 150) {
+      }
+      if (st < 150) {
         if (navbar.hasClass("scrolled")) {
-        navbar.removeClass("scrolled sleep");
+          navbar.removeClass("scrolled sleep");
         }
-    }
+      }
 
-        if (st > 350) {
+      if (st > 350) {
         if (!navbar.hasClass("awake")) {
-            navbar.addClass("awake");
+          navbar.addClass("awake");
         }
         if (sd.length > 0) {
-            sd.addClass("sleep");
+          sd.addClass("sleep");
         }
-    }
+      }
 
-        if (st < 350) {
+      if (st < 350) {
         if (navbar.hasClass("awake")) {
-            navbar.removeClass("awake");
-            navbar.addClass("sleep");
+          navbar.removeClass("awake");
+          navbar.addClass("sleep");
         }
         if (sd.length > 0) {
-            sd.removeClass("sleep");
+          sd.removeClass("sleep");
         }
-    }
+      }
     });
-    };
-    scrollWindow();
-    $.Scrollax();
+  };
+  scrollWindow();
+  $.Scrollax();
 
   //   carousel
-    let carousel = function () {
-        $(".home-slider").owlCarousel({
-        loop: true,
-        autoplay: true,
-        margin: 0,
-        animateOut: "fadeOut",
-        animateIn: "fadeIn",
-        nav: true,
-        dots: false,
-        autoplayHoverPause: false,
-        items: 1,
-        navText: [
-            "<span class = 'ion-ios-arrow-back'></span>",
-            "<span class = 'ion-ios-arrow-forward'></span>",
-        ],
-        responsive: {
-            0: {
-            items: 1,
-            },
-            600: {
-            items: 1,
-            },
-            1000: {
-            items: 1,
-            },
+  let carousel = function () {
+    $(".home-slider").owlCarousel({
+      loop: true,
+      autoplay: true,
+      margin: 0,
+      animateOut: "fadeOut",
+      animateIn: "fadeIn",
+      nav: true,
+      dots: false,
+      autoplayHoverPause: false,
+      items: 1,
+      navText: [
+        "<span class = 'ion-ios-arrow-back'></span>",
+        "<span class = 'ion-ios-arrow-forward'></span>",
+      ],
+      responsive: {
+        0: {
+          items: 1,
         },
-        });
-        $(".carousel-testimony").owlCarousel({
-            loop: true,
-            autoplay: true,
-            center: true,
-            margin: 30,
-            nav: false,
-            stagePadding: 0,
-            items: 1,
-            navText: [
-            "<span class = 'ion-ios-arrow-back'></span>",
-            "<span class = 'ion-ios-arrow-forward'></span>",
-            ],
-            responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2,
-            },
-            1000: {
-                items: 3,
-            },
-            },
-        });
-    };
-    carousel();
-
-    // Counter
-
-    let counter = function () {
-        $("#section-counter").waypoint(
-        function (direction) {
-            if (
-            direction === "down" &&
-            !$(this.element).hasClass("ftco-animated")
-            ) {
-            let comma_seperator_number_step = $.animateNumber.numberStepFactories.separator(
-                ","
-            );
-            $(".number").each(function () {
-                let $this = $(this),
-                num = $this.data("number");
-                console.log(num);
-                $this.animateNumber(
-                {
-                    number: num,
-                    numberStep: comma_seperator_number_step,
-                },
-                7000
-                );
-            });
-            }
+        600: {
+          items: 1,
         },
-        { offset: "95%" }
-        );
-    };
-    counter();
-    
-    let contentWayPoint = function () {
-        let i = 0;
-        $(".ftco-animate").waypoint(
-        function (direction) {
-            if (
-            direction === "down" &&
-            !$(this.element).hasClass("ftco-animated")
-            ) {
-            i++;
-            $(this.element).addClass("item-animate");
-            setTimeout(function () {
-                $("body .ftco-animate.item-animate").each(function (k) {
-                let el = $(this);
-                setTimeout(
-                    function () {
-                    let effect = el.data("animate-effect");
-                    if (effect === "fadeIn") {
-                        el.addClass("fadeIn ftco-animated");
-                    } else if (effect === "fadeInLeft") {
-                        el.addClass("fadeInLeft ftco-animated");
-                    } else if (effect === "fadeInRight") {
-                        el.addClass("fadeInRight ftco-animated");
-                    } else {
-                        el.addClass("fadeInUp ftco-animated");
-                    }
-                    el.removeClass("item-animate");
-                    },
-                    k * 50,
-                    "easeInOutExpo"
-                );
-                });
-            }, 100);
-            }
+        1000: {
+          items: 1,
         },
-        { offset: "95%" }
-        );
-    };
-    contentWayPoint();
-
-    $("#book_date").datepicker({
-        format: "m/d/yyyy",
-        autoclose: true,
+      },
     });
-    $("#book_time").timepicker();
+    $(".carousel-testimony").owlCarousel({
+      loop: true,
+      autoplay: true,
+      center: true,
+      margin: 30,
+      nav: false,
+      stagePadding: 0,
+      items: 1,
+      navText: [
+        "<span class = 'ion-ios-arrow-back'></span>",
+        "<span class = 'ion-ios-arrow-forward'></span>",
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        1000: {
+          items: 3,
+        },
+      },
+    });
+  };
+  carousel();
+
+  // Counter
+
+  let counter = function () {
+    $("#section-counter").waypoint(
+      function (direction) {
+        if (
+          direction === "down" &&
+          !$(this.element).hasClass("ftco-animated")
+        ) {
+          let comma_seperator_number_step =
+            $.animateNumber.numberStepFactories.separator(",");
+          $(".number").each(function () {
+            let $this = $(this),
+              num = $this.data("number");
+            // console.log(num);
+            $this.animateNumber(
+              {
+                number: num,
+                numberStep: comma_seperator_number_step,
+              },
+              7000
+            );
+          });
+        }
+      },
+      { offset: "95%" }
+    );
+  };
+  counter();
+
+  let contentWayPoint = function () {
+    let i = 0;
+    $(".ftco-animate").waypoint(
+      function (direction) {
+        if (
+          direction === "down" &&
+          !$(this.element).hasClass("ftco-animated")
+        ) {
+          i++;
+          $(this.element).addClass("item-animate");
+          setTimeout(function () {
+            $("body .ftco-animate.item-animate").each(function (k) {
+              let el = $(this);
+              setTimeout(
+                function () {
+                  let effect = el.data("animate-effect");
+                  if (effect === "fadeIn") {
+                    el.addClass("fadeIn ftco-animated");
+                  } else if (effect === "fadeInLeft") {
+                    el.addClass("fadeInLeft ftco-animated");
+                  } else if (effect === "fadeInRight") {
+                    el.addClass("fadeInRight ftco-animated");
+                  } else {
+                    el.addClass("fadeInUp ftco-animated");
+                  }
+                  el.removeClass("item-animate");
+                },
+                k * 50,
+                "easeInOutExpo"
+              );
+            });
+          }, 100);
+        }
+      },
+      { offset: "95%" }
+    );
+  };
+  contentWayPoint();
+
+  $("#book_date").datepicker({
+    format: "m/d/yyyy",
+    autoclose: true,
+  });
+  $("#book_time").timepicker();
 })(jQuery);
+
+let button = document.querySelector('.btn');
+let order = document.querySelector('form');
+let response = document.querySelector('.response-card');
+
+button.addEventListener("click", () => {
+  order.style.display = "none";
+  response.style.display = "relative";
+});
